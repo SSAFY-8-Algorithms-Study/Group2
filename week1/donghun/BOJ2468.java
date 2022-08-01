9bcd618d1136ea6e42c7146fcc015534b92c9e20
Donghun Lee <95545623+GIT-KINDMAN@users.noreply.github.com>
2022-08-02 02:47:12 +0900

    update: 1051

--- week1/donghun/BOJ2468.java
+++ week1/donghun/BOJ2468.java
@@ -1,5 +1,7 @@
 package week1.donghun;
 
 public class BOJ2468 {
+    public static void main(String[] args) {
 
+    }
 }
--- week1/donghun/BOJ2573.java
+++ week1/donghun/BOJ2573.java
@@ -1,5 +1,7 @@
 package week1.donghun;
 
 public class BOJ2573 {
+    public static void main(String[] args) {
 
+    }
 }
