d0828f69cf52dd9df7496dfcd6077790fe30cfe5
GIT-KINDMAN <mankind1990s@gmail.com>
2022-08-09 23:59:48 +0900

    fix: BOJ_2206_벽부수고이동하기(미완성)

--- week2/donghun/BOJ_2206_벽부수고이동하기.java
+++ week2/donghun/BOJ_2206_벽부수고이동하기.java
@@ -1,4 +1,25 @@
 package donghun;
 
+import java.util.*;
+import java.io.*;
+
 public class BOJ_2206_벽부수고이동하기 {
+    static int[] dx = {1, 0, -1, 0};
+    static int[] dy = {0, 1, 0, -1};
+    static int N, M;
+
+    static int[][] map;
+    boolean[][][] visited;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        map = new int[N][M];
+        boolean[][][] visit = new boolean[N][M][2];
+        Queue<int[]> qu = new LinkedList<>(); // BFS..?
+    }
 }
