a5fa781bcef316810c41ebd5392c5e9117188771
박서윤 <seoyoon528@gmail.com>
2022-09-07 18:20:45 +0900

    [Feat] 백준 10025:게으른 백곰

--- week6/seoyoon/BOJ10025.java
+++ week6/seoyoon/BOJ10025.java
@@ -6,6 +6,7 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
+/* 백준 10025:게으른 백곰 */
 public class BOJ10025 {
 
     public static void main(String[] args) throws IOException {
