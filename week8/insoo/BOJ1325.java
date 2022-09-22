85072e31888c7cab2e6b693a93f98438efcee46e
김인수 <kis877@naver.com>
2022-09-22 23:27:26 +0900

    S1 효율적인 해킹 _ AB 바꾸니 AC

--- week8/insoo/BOJ1325.java
+++ week8/insoo/BOJ1325.java
@@ -1,5 +1,3 @@
-package trying;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -9,7 +7,6 @@ import java.util.StringTokenizer;
 /*
  * BOJ 1325 Silver 1
  * 효율적인 해킹
- * 실패... - 시간초과
  */
 public class BOJ_1325_효율적인해킹 {
 	public static void main(String[] args) throws IOException {
@@ -31,14 +28,15 @@ public class BOJ_1325_효율적인해킹 {
 			for (int j = 0; st.hasMoreTokens(); j++) {
 				int A = Integer.parseInt(st.nextToken());
 				int B = Integer.parseInt(st.nextToken());
-				list[B].add(A);
+				list[A].add(B);
 			}
-		}
+		}	
+		
+		int[] cntList = new int[N+1];
 
 		for (int i = 1; i <= N; i++) {
 			boolean[] visited = new boolean[N+1];
 			visited[i] = true;
-			int cnt = 0;
 			
 			ArrayDeque<Integer> q = new ArrayDeque<>();
 			q.add(i);
@@ -46,23 +44,25 @@ public class BOJ_1325_효율적인해킹 {
 			while(!q.isEmpty()) {
 				int from = q.poll();
 				
-				for (int j = 0; j < list[from].size(); j++) {
-					int to = list[from].get(j);
+				for(int to : list[from]) {
 					if(visited[to]) continue;
 					if(from == to) continue;
 					visited[to] = true;
 					q.add(to);
-					cnt++;
+					cntList[to]++;
 				}
 			}
-			if(cnt >= max) {
-				if(cnt > max) {
-					sb = new StringBuilder();
-					max = cnt;
-				}
-				sb.append(i + " ");
-			}
+			
 		}
+		
+		for (int i = 1; i < cntList.length; i++) {
+			max = max > cntList[i] ? max : cntList[i];
+		}
+		
+		for (int i = 1; i < cntList.length; i++) {
+			if(max == cntList[i]) sb.append(i + " ");
+		}
+		
 		System.out.print(sb);
 	}
 }
