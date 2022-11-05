c492f28606389503cfcefb0923962e5f66ab3918
Donghun Lee <95545623+GIT-KINDMAN@users.noreply.github.com>
2022-11-05 10:39:00 +0900

    Update BOJ_11060_점프점프.java

--- week14/donghun/BOJ_11060_점프점프.java
+++ week14/donghun/BOJ_11060_점프점프.java
@@ -15,6 +15,7 @@ public class BOJ_11060_점프점프 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
+        // 불가능 Case: 출력 -1
         ans = -1;
         N = Integer.parseInt(br.readLine());
         arr = new int[N];
@@ -23,10 +24,14 @@ public class BOJ_11060_점프점프 {
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
+        
+        // 길이가 1이면 0회 시 가능
         if (N==1) {
             System.out.println(0);
             System.exit(0);
         }
+        
+        // 길이가 1인 경우는 걸러졌고, N>=2에서 0번 인덱스 값이 0이면 점프가 불가능
         if (arr[0] == 0) {
             System.out.println(-1);
             System.exit(0);
@@ -37,6 +42,7 @@ public class BOJ_11060_점프점프 {
 
         bfs();
 
+        // 출력 분기
         System.out.println(ans==-1?-1:ans+1);
     }
 
@@ -58,6 +64,7 @@ public class BOJ_11060_점프점프 {
                 int val = arr[idx];
                 if (val == 0) continue;
 
+                // arr[idx]에 주어진 값만큼 모두 돌려주고 queue add
                 for (int i = 1; i <= val; i++) {
                     int nextIdx = idx + i;
                     if (nextIdx < 0 || nextIdx >= N) continue;
@@ -67,8 +74,10 @@ public class BOJ_11060_점프점프 {
                     }
                 }
             }
+            // count 증가
             ans++;
         }
+         // 불가능 Case: 출력 -1
         ans = -1;
     }
 
