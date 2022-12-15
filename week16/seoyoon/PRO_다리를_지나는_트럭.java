0e84dfc2ef798085e1dad9ee67cdb2f4d6c6e96f
sypark <55688405+seoyoon528@users.noreply.github.com>
2022-12-15 20:49:45 +0900

    Update PRO_다리를_지나는_트럭.java

--- week16/seoyoon/PRO_다리를_지나는_트럭.java
+++ week16/seoyoon/PRO_다리를_지나는_트럭.java
@@ -10,21 +10,21 @@ public class PRO_다리를_지나는_트럭 {
     }
 
     private static int solution(int bridge_length, int weight, int[] truck_weights) {
-        int secCnt = 1;			// 경과 시간
+        int secCnt = 1;         // 경과 시간
         int curTruckIdx = 0;	// truck_weights[] 배열에서의 다리에 들어갈 트럭 인덱스
         int outTruckCnt = 0;	// 빠져나간 트럭 수 count
 
         Queue<Truck> q = new ArrayDeque<Truck>();
         q.add(new Truck(truck_weights[curTruckIdx], bridge_length));	// 첫번째 트럭 다리로 출발~
-        int truckCnt = 1;		// 현재 다리 위에 있는 트럭 수
-        int restWeight = weight - truck_weights[curTruckIdx++];			// 다리가 견딜 수 있는 남은 무게
+        int truckCnt = 1;                                               // 현재 다리 위에 있는 트럭 수
+        int restWeight = weight - truck_weights[curTruckIdx++];         // 다리가 견딜 수 있는 남은 무게
 
         while (true) {
-            q.forEach((truck) -> truck.restDist--);		// 다리에 올라가있는 모든 트럭의 남은 거리 -1
-            secCnt++;									// 경과시간 +1
+            q.forEach((truck) -> truck.restDist--);                     // 다리에 올라가있는 모든 트럭의 남은 거리 -1
+            secCnt++;                                                   // 경과시간 +1
 
-            if (q.peek().restDist == 0) {				// 제일 앞에있는 트럭의 남은 거리가 0이라면 빠져나가기
-                truckCnt--;								// 빠져나간 트럭만큼 원상복구
+                if (q.peek().restDist == 0) {                           // 제일 앞에있는 트럭의 남은 거리가 0이라면 빠져나가기
+                truckCnt--;                                             // 빠져나간 트럭만큼 원상복구
                 outTruckCnt++;
                 restWeight += q.peek().weight;
                 q.poll();
@@ -34,9 +34,9 @@ public class PRO_다리를_지나는_트럭 {
                 break;
             }
 
-            if (curTruckIdx < truck_weights.length) {	// truck 배열의 범위를 벗어나지 않고
-                if (truckCnt < bridge_length && restWeight - truck_weights[curTruckIdx] >= 0) {		// 현재 다리에 올라간 트럭의 수가 bridge_length보다 작으면서 + 다리에 올라갈 새로운 트럭의 무게를 다리가 견딜 수 있다면
-                    q.add(new Truck(truck_weights[curTruckIdx], bridge_length));					// 다리에 올려주기
+            if (curTruckIdx < truck_weights.length) {	                                            // truck 배열의 범위를 벗어나지 않고
+                if (truckCnt < bridge_length && restWeight - truck_weights[curTruckIdx] >= 0) {     // 현재 다리에 올라간 트럭의 수가 bridge_length보다 작으면서 + 다리에 올라갈 새로운 트럭의 무게를 다리가 견딜 수 있다면
+                    q.add(new Truck(truck_weights[curTruckIdx], bridge_length));                    // 다리에 올려주기
 
                     restWeight -= truck_weights[curTruckIdx];
                     truckCnt++;
