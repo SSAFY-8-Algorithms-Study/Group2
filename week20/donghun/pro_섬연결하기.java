62dae44a7d3a402d678871a698d02fafa0b73850
Donghun Lee <95545623+GIT-KINDMAN@users.noreply.github.com>
2023-01-12 20:38:52 +0900

    Create pro_섬연결하기.java
    정신이 멍해진다... 프림 어떻게 짜는거였지

---
+++ week20/donghun/pro_섬연결하기.java
@@ -0,0 +1,21 @@
+package temp_pro.week20;
+
+import java.util.*;
+import java.io.IOException;
+
+public class pro_섬연결하기 {
+    public static void main(String[] args) throws NumberFormatException, IOException {
+
+        // 출력
+        pro_섬연결하기 S = new pro_섬연결하기();
+        System.out.println(S.solution(4, new int[][]{{0,1,1},{0,2,2},{1,2,5},{1,3,1},{2,3,8}})); // 4
+    }
+
+    public int solution(int n, int[][] costs) {
+        int answer = 0;
+
+        // 모든 노드 연결 -> 크루스칼 or 프림 알고리즘 사용
+
+        return answer;
+    }
+}
