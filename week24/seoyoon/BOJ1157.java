fa09a1bc836465ed262a756f90dcb1630c47e43e
박서윤 <seoyoon528@gmail.com>
2023-02-23 00:48:18 +0900

    [Feat] 백준 1157 : 단어 공부

---
+++ week24/seoyoon/BOJ1157.java
@@ -0,0 +1,34 @@
+package week24.seoyoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/* 단어 공부 */
+public class BOJ1157 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String word = br.readLine().toUpperCase();
+
+        int max = 0, idx = 0;
+        char maxAlpha = 0;
+        boolean flag = false;
+
+        int[] alpha = new int[26];
+        for (int i = 0; i < word.length(); i++) {
+            idx = word.charAt(i) - 65;
+            alpha[idx]++;
+
+            if (alpha[idx] > max) {
+                max = alpha[idx];
+                maxAlpha = word.charAt(i);
+                flag = false;
+            }
+            else if (alpha[idx] == max) {
+                flag = true;
+            }
+        }
+
+        System.out.println(flag ? "?" : maxAlpha);
+    }
+}
